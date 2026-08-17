1cd28c471fb2ccef3e995d7b9087a2feb521b51c
drmohamedalaa90 <drmohamedalaa90@gmail.com>
2026-08-17 20:30:21 +0300

    Load v194 Meetings refresh state

--- assets/schedule-meetings-unified-v168.js
+++ assets/schedule-meetings-unified-v168.js
@@ -1,4 +1,4 @@
 import "./attendance-camera-checkin-v192.js?v=192";
 import "./physical-camera-ui-v192.js?v=192";
 import "./schedule-meetings-shell-v187.js?v=191";
-import "./schedule-meetings-refresh-state-v193.js?v=193";
+import "./schedule-meetings-refresh-state-v193.js?v=194";
