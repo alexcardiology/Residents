cc038c66ae6af5b71970a55175338ea7ebf6ffd8
drmohamedalaa90 <drmohamedalaa90@gmail.com>
2026-08-17 20:43:18 +0300

    Load meeting admin enhancements

--- assets/schedule-meetings-unified-v168.js
+++ assets/schedule-meetings-unified-v168.js
@@ -2,3 +2,4 @@ import "./attendance-camera-checkin-v192.js?v=192";
 import "./physical-camera-ui-v192.js?v=192";
 import "./schedule-meetings-shell-v187.js?v=191";
 import "./schedule-meetings-refresh-state-v193.js?v=194";
+import "./meeting-admin-enhancements-v194.js?v=194";
