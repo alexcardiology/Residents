2cdc6fc15f982752cbbf69e4df9578784e8a3ac1
drmohamedalaa90 <drmohamedalaa90@gmail.com>
2026-08-17 16:47:52 +0300

    Route v173 compatibility imports to v175 runtime gateway

--- assets/supabase-v173.js
+++ assets/supabase-v173.js
@@ -4,5 +4,6 @@ export {
   SUPABASE_URL,
   SUPABASE_PUBLISHABLE_KEY,
   SUPABASE_LEGACY_ANON_KEY,
+  SUPABASE_BROWSER_KEY,
   createLegacyFallbackClient,
-} from "./supabase-v174.js";
+} from "./supabase-v175.js";
